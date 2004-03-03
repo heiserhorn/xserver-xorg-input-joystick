b02ae094bcc146f2a6ba601c28bc3541663fdaa0
Egbert Eich <eich@suse.de>
2004-03-03 12:12:33 +0000

    Importing vendor version xf86-4_4_0 on Wed Mar 3 04:09:24 PST 2004

--- src/xf86Jstk.c
+++ src/xf86Jstk.c
@@ -22,7 +22,7 @@
  *
  */
 
-/* $XFree86: xc/programs/Xserver/hw/xfree86/input/joystick/xf86Jstk.c,v 1.6 2001/12/26 21:51:59 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/input/joystick/xf86Jstk.c,v 1.5 2001/11/26 16:25:53 dawes Exp $ */
 
 #include <xf86Version.h>
 
