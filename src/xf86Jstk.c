865d0119123f13b8f774036d0d736448b939fce0
Daniel Stone <daniel@fooishbar.org>
2005-07-01 22:43:22 +0000

    Change all misc.h and os.h references to <X11/foo.h>.

--- src/xf86Jstk.c
+++ src/xf86Jstk.c
@@ -61,7 +61,7 @@
 #define NEED_EVENTS
 #include <X11/X.h>
 #include <X11/Xproto.h>
-#include "misc.h"
+#include <X11/misc.h>
 #include "inputstr.h"
 #include "scrnintstr.h"
 #include <X11/extensions/XI.h>
