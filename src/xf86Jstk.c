409ae82279caa2719ac88d6f134e3bbed018e9ab
Drew Parsons <dparsons@debian.org>
2006-10-10 13:33:43 +0000

    New upstream versions (X11R7.1): xf86-input-elo2300-1.1.0 xf86-input-elographics-1.1.0 xf86-input-fpit-1.1.0 xf86-input-jamstudio-1.1.0 xf86-input-joystick-1.1.0

--- src/xf86Jstk.c
+++ src/xf86Jstk.c
@@ -39,7 +39,6 @@
 
 #include <misc.h>
 #include <xf86.h>
-#include <xf86_ansic.h>
 #include <xf86_OSproc.h>
 #include <xf86Xinput.h>
 #include <xisb.h>
@@ -775,7 +774,7 @@ static XF86ModuleVersionInfo xf86JstkVersionRec =
 	MODINFOSTRING1,
 	MODINFOSTRING2,
 	XORG_VERSION_CURRENT,
-	1, 0, 0,
+	1, 1, 0,
 	ABI_CLASS_XINPUT,
 	ABI_XINPUT_VERSION,
 	MOD_CLASS_XINPUT,
